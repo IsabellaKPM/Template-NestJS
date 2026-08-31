c3d23a79d1b2adfcc78b60a2d5a1f8c62d19511c
IsabellaKPM <ikpena@uc.cl>
2026-08-31 01:15:31 -0400

    fix(api-constants): split cors_origins by default

--- src/core/constants/api.constant.ts
+++ src/core/constants/api.constant.ts
@@ -2,5 +2,5 @@ export const API_CONFIG = {
   PREFIX: "api",
   HOST: process.env.HOST || "0.0.0.0",
   PORT: Number(process.env.PORT) || 3001,
-  CORS_ORIGINS: process.env.CORS_ORIGINS,
+  CORS_ORIGINS: process.env.CORS_ORIGINS?.split(",") || [],
 } as const;
