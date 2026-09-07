8f60570d03ed03e86ba30974c842030d338e8861
IsabellaKPM <ikpena@uc.cl>
2026-09-06 22:26:45 -0300

    ci(config): asset type on app config

--- src/infrastructure/config/configs/app.config.ts
+++ src/infrastructure/config/configs/app.config.ts
@@ -3,7 +3,7 @@ import { isProduction } from "../helpers/env.helpers";
 
 export const appConfig = registerAs("app", () => ({
   port: Number(process.env.PORT),
-  host: process.env.HOST,
+  host: process.env.HOST as string,
   corsOrigins: process.env.CORS_ORIGINS?.split(",") ?? [],
   nodeEnv: process.env.NODE_ENV,
   isProduction: isProduction(),
